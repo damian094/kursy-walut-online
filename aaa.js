a1b71c3841e7f4fd6a280e4d485d902e46fcb161
damian094 <damian_094@wp.pl>
2020-12-11 17:14:46 +0100

    Add new files

--- aaa.js
+++
@@ -1,8 +0,0 @@
-import superagent from 'superagent';
-
-superagent
-.get('http://api.nbp.pl/api/exchangerates/rates/A/EUR/?format=json')
-.set('accept', 'json')
-.end((err, res)=>{
-    console.log(res.status, res.body.code, res)
-})
